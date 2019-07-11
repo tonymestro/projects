60cf9efdab7b781255c6c9ae4777c6ef24852185
tonymestro <51722742+tonymestro@users.noreply.github.com>
2019-07-11 15:08:55 -0300

    Add files via upload
    Opção de leitura RFID - Faz várias leituras e envia dados via GET

---
+++ lanches_2.ino
@@ -0,0 +1,111 @@
+/*
+ * HTTP Client GET Request
+ * Copyright (c) 2018, circuits4you.com
+ * All rights reserved.
+ * https://circuits4you.com 
+ * Connects to WiFi HotSpot. */
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN    D3    
+#define SS_PIN     D4   
+#define LED        D1
+
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
+
+/* Set these to your desired credentials. */
+const char *ssid = "IFFarroupilha";  //ENTER YOUR WIFI SETTINGS
+const char *password = "";
+
+//Web/Server address to read/write from 
+const char *host = "192.168.20.43";   //https://circuits4you.com website or IP address of server
+
+//=======================================================================
+//                    Power on setup
+//=======================================================================
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  SPI.begin();      // Inicia  SPI bus
+  mfrc522.PCD_Init();   // Inicia MFRC522
+  WiFi.mode(WIFI_OFF);        //Prevents reconnection issue (taking too long to connect)
+  delay(1000);
+  WiFi.mode(WIFI_STA);        //This line hides the viewing of ESP as wifi hotspot
+  
+  WiFi.begin(ssid, password);     //Connect to your WiFi router
+  Serial.println("");
+
+  Serial.print("Connecting");
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  //If connection successful show IP address in serial monitor
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+}
+
+//=======================================================================
+//                    Main Program Loop
+//=======================================================================
+void loop() {
+  HTTPClient http;    //Declare object of class HTTPClient
+
+  String ADCData, station, getData, Link;
+  
+  //ADCData = String(adcvalue);   //String to interger conversion
+  //station = "B";
+
+  // Procura por cartao RFID
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Seleciona o cartao RFID
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Mostra UID na serial
+  Serial.print("UID da tag :");
+  String tag= "";
+  byte letra;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     tag.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : ""));
+     tag.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Mensagem : ");
+  tag.toUpperCase();
+
+  //ENVIO DE DADOS VIA GET
+ 
+  Link = "http://192.168.20.43/github/projects/reserva_tag.php?tag=" + tag;
+  
+  http.begin(Link);     //Specify request destination
+  
+  int httpCode = http.GET();            //Send the request
+  String payload = http.getString();    //Get the response payload
+
+  Serial.println(httpCode);   //Print HTTP return code
+  Serial.println(payload);    //Print request response payload
+
+  http.end();  //Close connection
+  
+  delay(5000);  //GET Data at every 5 seconds
+}
+//======================================================================
